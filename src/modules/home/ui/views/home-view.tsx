96413725184ab99aaeac70e4f0317637110054b7
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2025-06-30 21:44:08 +0530

    trpc_setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -2,31 +2,18 @@
 
 import { Button } from "@/components/ui/button"
 import { authClient } from "@/lib/auth-client"
+import { useTRPC } from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
 
 export const HomeView = () => {
-  const router = useRouter()
-  const { data: session } = authClient.useSession()
-
-  if (!session) {
-    return <h1>Loading.....</h1>
-  }
+  const trpc = useTRPC()
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Ajay" }))
 
   return (
     <div>
       Home Page (protected)
-      <h1>Logged in as {session?.user.name}</h1>
-      {!!session && (
-        <Button
-          onClick={() =>
-            authClient.signOut({
-              fetchOptions: { onSuccess: () => router.push("/sign-in") },
-            })
-          }
-        >
-          logout
-        </Button>
-      )}
+      <h1>Logged in as {data?.greeting}</h1>
     </div>
   )
 }
