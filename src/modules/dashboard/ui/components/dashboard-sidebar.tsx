278abda9dd4bff909ec1bf0343b80a9f8efb7647
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2025-06-30 19:46:09 +0530

    typo_fix

--- src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -21,7 +21,7 @@ import { DashboardUserButton } from "./dashboard-user-button"
 const firstSection = [
   {
     icon: Video,
-    label: "Meetigs",
+    label: "Meetings",
     href: "/meetings",
   },
   {
@@ -72,7 +72,7 @@ export const DashboardSidebar = () => {
                   key={item.href}
                   className={cn(
                     "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
-                    pathname === item.href && "bg-linear-t0-r/oklch border-[#5D6B68]/10"
+                    pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10"
                   )}
                   isActive={pathname === item.href}
                 >
